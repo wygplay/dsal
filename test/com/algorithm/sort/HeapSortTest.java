ed61903941e6ef04e1b0936587893ab9d7e973f0
wygplay <vipwyg@163.com>
2021-05-15 09:44:54 +0800

    堆排序-复习

--- test/com/algorithm/sort/HeapSortTest.java
+++ test/com/algorithm/sort/HeapSortTest.java
@@ -3,6 +3,7 @@ package com.algorithm.sort;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.notification.RunListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -33,8 +34,41 @@ public class HeapSortTest {
         HeapSort.sort(arr);
     }
 
+    @Test
+    public void sort() {
+        //初始化堆
+        for (int i = arr.length / 2 -1; i >=0; i--) {
+            adjustHeapSort(arr, i, arr.length);
+        }
+        //交换与重构
+        int temp = 0;
+        for (int j = arr.length - 1; j > 0; j--) {
+            temp = arr[0];
+            arr[0] = arr[j];
+            arr[j] = temp;
+            adjustHeapSort(arr, 0, j);
+        }
+    }
+
+    public void adjustHeapSort(int[] arr, int i, int length) {
+        int temp = arr[i];
+        for (int k = 2 * i + 1; k < length; k = 2 * i + 1) {
+            if (k + 1 < length && arr[k] < arr[k + 1]) {
+                k++;
+            }
+            if (arr[k] > temp) {
+                arr[i] = arr[k];
+                i = k;
+            } else {
+                break;
+            }
+        }
+        arr[i] = temp;
+    }
+
     @After
     public void print() {
         System.out.println(Arrays.toString(arr));
     }
+
 }
