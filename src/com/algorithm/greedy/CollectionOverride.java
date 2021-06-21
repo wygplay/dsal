1e0870bcbd5d7ffe64fb1c94239fa0da9140419b
wangyg <wangyg8@asiainfo.com>
2021-06-21 21:55:44 +0800

    贪心算法-集合覆盖问题-空间复杂度优化

--- src/com/algorithm/greedy/CollectionOverride.java
+++ src/com/algorithm/greedy/CollectionOverride.java
@@ -26,21 +26,24 @@ public class CollectionOverride {
     }
 
     public static void greedy(Set<String> locations, Map<String, Set<String>> itemLocation, List<String> finalLocations) {
+        String bestLocation;
+        Set<String> covered = new HashSet<>();
         while(!locations.isEmpty()) {
-            System.out.println("------");
-            String bestLocation = null;
-            Set<String> covered = new HashSet<>();
+            bestLocation = null;
             int bestLocationCoveredSize = 0;
+            // 每一次for循环，选出与当前未选择的locations交集最大的选项，即贪心算法
             for (Map.Entry<String, Set<String>> entry : itemLocation.entrySet()) {
                 covered.clear();
                 covered.addAll(entry.getValue());
                 covered.retainAll(locations);
+                // bestLocation == null为每轮初始化的问题，bestLocationCoveredSize记录截止到当前选项时，选出的bestLocation集合与locations的交集大小
                 if (bestLocation == null || bestLocationCoveredSize < covered.size()) {
                     bestLocation = entry.getKey();
                     bestLocationCoveredSize = covered.size();
                 }
             }
             finalLocations.add(bestLocation);
+            // 去除本轮已经选择的，以便下次选择时，排除此项干扰
             locations.removeAll(itemLocation.get(bestLocation));
         }
     }
